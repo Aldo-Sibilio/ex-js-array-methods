c31987eb5d63e7f8d46be013c57855125b13d2c7
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-04-14 15:52:50 +0200

    risolto snack 3 con map

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,12 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+
+// creo nuovo array con numeri +1
+const newNumbers = numbers.map(function(numero) {
+  return numero + 1;
+});
+
+console.log(newNumbers);
+
 // Risultato: [3, 9, 5, 8, 3, 88]
